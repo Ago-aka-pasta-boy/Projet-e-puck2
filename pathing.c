c91e4be15be5e598fc1f09601fe3a0a582907034
sylvain <sylvain.pellegrini@epfl.ch>
2020-05-10 20:52:34 +0200

    started cleanup of pathing.c/.h

--- pathing.c
+++ pathing.c
@@ -19,9 +19,37 @@
 #include <audio_processing.h>
 #include "leds.h"
 
+//Defines
+#define ROT_COEF 				7			//Experimental value, ROT_COEF * angle gives the speed at which to turn during ROT_TIME
+#define ROT_TIME				500			//[ms] Half a second is needed to have big angles
+#define SAFETY_DISTANCE 		40			//[mm] The robot will go into reverse if he sees anything closer
+#define MAX_TRAVEL_TIME			3000		//[ms] Maximum amount of time the robot will go forward without reorientating itself
+#define MAX_ROT_TIME 			3000		//[ms] Maximum amount of time the robot will spend trying to orientate itself
+#define COLLISION_DISTANCE 		30			//[mm] Distance at which the robot considers he collided with the gate he was ramming
+#define MINIMUM_ROT_SPEED		175			//Turning at a lower speed starts to make the robot shake instead of turning properly
+#define SLOW_DOWN_DISTANCE		200			//[mm] The robot will slow down at that distance to facilitate the obstacle recognition
+#define SLOW_SPEED				300			//[step/s]
+#define FAST_SPEED				500			//[step/s]
+#define HALT					0			//[step/s]
+#define	LARGE_ANGLE				40			//[degrees] Angle after which the past average of the filter will be reset to fix long convergence times
+#define STABILIZATION_TRIES		5			//Maximum amount of times the angle is polled to determine its stability
+#define STABILIZED_AUDIO		2			//The angle will be considered stable if more than 2 of the tries are accepted
+#define MAX_ANGLE_ERROR			15.0f		//[degrees] The maximum error of the sound localization accuracy
+#define RAM_SPEED				1000		//[step/s]
+#define CELEBRATION_TIME		1000		//[ms]
+#define TOO_CLOSE				80			//[mm] The robot will reach this distance when moving back
+#define MAX_POS_SHIFT			50			//[nb of pixels] The maximum accepted difference of position between two instances of the same obstacle
+#define MAX_DIST_TO_CONSIDER	100			//[mm] Obstacles further than this will not be taken into consideration
+#define THRESHOLD_EDGE_AND_GOAL 4			//Count of consecutive instances of the same obstacle to confirm it
+#define THRESHOLD_GATE			2			//Count of consecutive instances of a gate to confirm it
+#define OBSTACLE_CLEARING_DELAY	500			//[ms] Amount of time needed to complete the rotation
+#define SENSOR_REFRESH_DELAY	100			//[ms] Minimum amount of time between new values dictated by the ToF sensor thread
+
 static uint8_t last_type=0, count=0;
 static uint16_t last_pos=0;
 
+
+//Short function to move the robot straight
 void set_speed(int speed)
 {
 	left_motor_set_speed(speed);
@@ -32,6 +60,7 @@ void set_speed(int speed)
 //Short sign(x) function
 int sign(float x){return (x > 0) - (x < 0);}
 
+//Short function to rotate the robot
 void rotate_lr(int lr)
 {
 	if(abs(lr)<MINIMUM_ROT_SPEED){lr =sign(lr)*MINIMUM_ROT_SPEED;}
@@ -39,7 +68,7 @@ void rotate_lr(int lr)
 	right_motor_set_speed(-lr);
 }
 
-
+//Routine executed when the obstacle is of type UNKNOWN
 void move_back (void)
 {
 	set_led(LED1, 1);
@@ -53,7 +82,8 @@ void move_back (void)
 }
 
 
-uint8_t recognize_obstacle(void)
+//Function to identify and confirm obstacles by looking at the data multiple times, the type of the obstacle is stored in last_type
+bool recognize_obstacle(void)
 {
 	if (get_obstacle_type() && get_obstacle_type()!=UNKNOWN)
 	{
@@ -66,21 +96,22 @@ uint8_t recognize_obstacle(void)
 		last_type = get_obstacle_type();
 		last_pos = get_obstacle_pos();
 
-		if ((count>THRESHOLD_EDGE_AND_GOAL || (count>THRESHOLD_GATE && last_type == GATE)) && VL53L0X_get_dist_mm() < MAX_DIST_TO_CONSIDER)
+		if ((count>=THRESHOLD_EDGE_AND_GOAL || (count>=THRESHOLD_GATE && last_type == GATE)) && VL53L0X_get_dist_mm() < MAX_DIST_TO_CONSIDER)
 		{
-			return last_type;
+			return TRUE;
 		}
 	}
 	return FALSE;
 }
 
+//Function to move forward and look for obstacles, returns the type of the obstacle if any has been found
 uint8_t path_to_obstacle (void)
 {
 	systime_t start_time=chVTGetSystemTime();
 
 	set_body_led(1);
 
-	while(chVTGetSystemTime()<start_time + TRAVEL_TIME)
+	while(chVTGetSystemTime()<start_time + MAX_TRAVEL_TIME)
 	{
 		//Releases resources to the sensor handling threads to update
 		chThdSleepMilliseconds(SENSOR_REFRESH_DELAY);
@@ -89,7 +120,7 @@ uint8_t path_to_obstacle (void)
 		{
 			set_speed(HALT);
 			set_body_led(0);
-			return last_type;
+			return last_type;			//The type recognized was stored in the static variable last_type
 		}
 
 		if(VL53L0X_get_dist_mm()<SAFETY_DISTANCE)
@@ -113,7 +144,7 @@ uint8_t path_to_obstacle (void)
 	return FALSE;
 }
 
-
+//Function to rotate toward the source and look for obstacles, returns the type of the obstacle if any has been found
 uint8_t rotate_to_source (void)
 {
 	float turnangle = 0;
@@ -131,9 +162,9 @@ uint8_t rotate_to_source (void)
 		check_angle=0;
 		turnangle = get_angle();
 		rotate_lr(ROT_COEF*turnangle);
-		chThdSleepMilliseconds(500);
+		chThdSleepMilliseconds(ROT_TIME);
 		set_speed(HALT);
-		if (chVTGetSystemTime()>start_time + ROT_TIME){break;}
+		if (chVTGetSystemTime()>start_time + MAX_ROT_TIME){break;}
 		if (fabs(turnangle)>LARGE_ANGLE){reset_audio();  chThdSleepMilliseconds(1500);}
 		chThdSleepMilliseconds(1000);
 
